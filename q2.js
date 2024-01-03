667552cb89c626d3eb4e078871628e1246a163af
jamilekarinee <139991955+jamilekarinee@users.noreply.github.com>
2024-01-03 12:10:50 -0300

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,12 @@
+// capitalizarTitulos
+
+
+const capitalizarTitulos = (arrayDeStrings) => arrayDeStrings.map(string => string.toUpperCase());
+
+console.log(capitalizarTitulos(["o pequeno principe","dom casmurro","a metamorfose"]));// length[i] 
+//.split(\)
+
+// Tentei realizar esta questões de muitas formas, até com 
+// return livros[0].[0].toUpperCase(); 
+// mas não consegui. Tem como o senhor postar as respostas depois, por gentileza? 
+
